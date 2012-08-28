5131464ed45b738b5387a1647ff4d8fa21da2573
Jeffrey Way <jeffrey@envato.com>
2012-08-28 17:28:10 -0400

    Clean up test file

--- test/MessageBox_test.js
+++ test/MessageBox_test.js
@@ -3,25 +3,6 @@
 /*global notDeepEqual:false, strictEqual:false, notStrictEqual:false, raises:false*/
 (function($) {
 
-  /*
-    ======== A Handy Little QUnit Reference ========
-    http://docs.jquery.com/QUnit
-
-    Test methods:
-      expect(numAssertions)
-      stop(increment)
-      start(decrement)
-    Test assertions:
-      ok(value, [message])
-      equal(actual, expected, [message])
-      notEqual(actual, expected, [message])
-      deepEqual(actual, expected, [message])
-      notDeepEqual(actual, expected, [message])
-      strictEqual(actual, expected, [message])
-      notStrictEqual(actual, expected, [message])
-      raises(block, [expected], [message])
-  */
-
   module('jQuery.message', {
     setup: function() {
       this.msg = $.message('Are you sure?');
@@ -96,41 +77,5 @@
 
     equal($('#qunit-fixture').text(), 'The value is: Yes');
   });
-
-
-
-
-
-  // module('jQuery#awesome', {
-  //   setup: function() {
-  //     this.elems = $('#qunit-fixture').children();
-  //   }
-  // });
-
-  // test('is chainable', 1, function() {
-  //   // Not a bad test to run on collection methods.
-  //   strictEqual(this.elems.awesome(), this.elems, 'should be chaninable');
-  // });
-
-  // test('is awesome', 1, function() {
-  //   strictEqual(this.elems.awesome().text(), 'awesomeawesomeawesome', 'should be thoroughly awesome');
-  // });
-
-  // module('jQuery.awesome');
-
-  // test('is awesome', 1, function() {
-  //   strictEqual($.awesome(), 'awesome', 'should be thoroughly awesome');
-  // });
-
-  // module(':awesome selector', {
-  //   setup: function() {
-  //     this.elems = $('#qunit-fixture').children();
-  //   }
-  // });
-
-  // test('is awesome', 1, function() {
-  //   // Use deepEqual & .get() when comparing jQuery objects.
-  //   deepEqual(this.elems.filter(':awesome').get(), this.elems.last().get(), 'knows awesome when it sees it');
-  // });
-
+  
 }(jQuery));
