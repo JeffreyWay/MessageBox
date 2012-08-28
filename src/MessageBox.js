4c436074890c21146b2f439ad654b478ba25f197
Jeffrey Way <jeffrey@envato.com>
2012-08-28 14:40:23 -0400

    Grunt init

---
+++ src/MessageBox.js
@@ -0,0 +1,28 @@
+/*
+ * MessageBox
+ * https://github.com/JeffreyWay/MessageBox
+ *
+ * Copyright (c) 2012 Jeffrey Way
+ * Licensed under the GPL license.
+ */
+
+(function($) {
+
+  // Collection method.
+  $.fn.awesome = function() {
+    return this.each(function() {
+      $(this).html('awesome');
+    });
+  };
+
+  // Static method.
+  $.awesome = function() {
+    return 'awesome';
+  };
+
+  // Custom selector.
+  $.expr[':'].awesome = function(elem) {
+    return elem.textContent.indexOf('awesome') >= 0;
+  };
+
+}(jQuery));
